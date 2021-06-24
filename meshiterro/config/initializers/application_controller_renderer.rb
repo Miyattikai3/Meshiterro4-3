10c1e6d8b246e622bdb46b0f71b451efa600bf8b
Miyattikai3 <miyatchik@gmail.com>
2021-06-24 05:32:35 +0000

    [meshiterro]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b99c3d4d8ae9bb08ce733aadfc2db6db0c8e80392f27ce7ffc8857e0c903e31098f6691790cfbb3818ef5b171699f9b1e7f4a2bfcb31f1acfd37181207e92eb2'
